de403b451b6ba67e714209148ca01e9d97482e38
CASEY BEHRENDT <behrendtc@gmail.com>
2022-06-30 17:19:54 -0600

    routes added

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -1,2 +1,13 @@
 class ActorController < ApplicationController
+
+  def all_actors
+    actors = Actor.all
+    render json: actors.as_json
+  end
+
+  def "actor"
+    first_actor = actor.first
+    render json: actor.as_json
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/all_actors", controller "actor", action: "all_actors"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/actor", controller: "actor", action: "first_actor"
 end
