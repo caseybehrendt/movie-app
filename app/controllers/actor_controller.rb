95964fbadd96a17796e804a0831639f4dc16da7d
CASEY BEHRENDT <behrendtc@gmail.com>
2022-07-06 16:37:59 -0600

    updated movie controller

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -1,5 +1,4 @@
 class ActorController < ApplicationController
-
   def actors
     actors = Actor.all
     render json: actors.as_json
@@ -12,5 +11,33 @@ class ActorController < ApplicationController
 
   def show
     actor = Actor.find_by(id: params["id"])
-  
+    render json: actor.as_json
+  end
+
+  def create
+    actor = Actor.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      known_for: params["known_for"],
+    )
+  end
+
+  def update
+    actor_id = params["id"]
+    actor = Actor.find_by(id: actor_id)
+
+    actor.first_name = params["first_name"] || actor.first_name
+    actor.last_name = params["last_name"] || actor.last_name
+    actor.known_for = params["known_for"] || actor.known_for
+
+    actor.save
+    render json: actor.as_json
+  end
+
+  def destroy
+    actor_id = params["id"]
+    actor = Actor.find_by(id: actor_id)
+    actor.destroy
+    render json: { message: "Actor record has been successfully deleted" }
+  end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,46 @@ class MovieController < ApplicationController
     first_movie = Movie.first
     render json: actor.as_json
   end
+
+  def movies
+    movies = Movie.all
+    render json: movies.as_json
+  end
+
+  def movie
+    title = Movie.first
+    render json: title.as_json
+  end
+
+  def show
+    movie = Movie.find_by(id: params["id"])
+    render json: movie.as_json
+  end
+
+  def create
+    movie = Movie.new(
+      title: params["title"],
+      year: params["year"],
+      plot: params["plot"],
+    )
+  end
+
+  def update
+    movie_id = params["id"]
+    movie = Movie.find_by(id: movie_id)
+
+    movie.title = params["title"] || movie.title
+    movie.year = params["year"] || movie.year
+    movie.plot = params["plot"] || movie.plot
+
+    movie.save
+    render json: movie.as_json
+  end
+
+  def destroy
+    movie_id = params["id"]
+    movie = Movie.find_by(id: movie_id)
+    movie.destroy
+    render json: { message: "Movie record has been successfully deleted" }
+  end
 end
