a3e477dc013a26d3e85f6ee73aee035161788563
CASEY BEHRENDT <behrendtc@gmail.com>
2022-07-19 07:57:19 -0600

    adds genre and movie genre models

--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,6 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_220118) do
-  # These are extensions that must be enabled in order to support this database
+
+ActiveRecord::Schema[7.0].define(version: 2022_07_19_134510) do
   enable_extension "plpgsql"
 
   create_table "actors", force: :cascade do |t|
@@ -32,6 +21,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_220118) do
 
   create_table "adds_english_to_moves", force: :cascade do |t|
     t.string "english"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movie_genres", force: :cascade do |t|
+    t.integer "genre_id"
+    t.integer "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
